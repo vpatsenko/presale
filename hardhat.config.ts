92c1a70dfce448c01e403473b5ad2f92b381500e
Vladimir Patsenko <v@patsenko.xyz>
2025-05-31 21:10:52 +0300

    deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,10 +26,7 @@ const config: HardhatUserConfig = {
     },
   },
   etherscan: {
-    apiKey: {
-      base: process.env.ETHERSCAN_API_KEY || "",
-      baseSepolia: process.env.ETHERSCAN_API_KEY || "",
-    },
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
     customChains: [
       {
         network: "base",

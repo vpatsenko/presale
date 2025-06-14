4d2cfede902dad1c3efe9b69471965d6c6317535
Vladimir Patsenko <v@patsenko.xyz>
2025-06-14 18:51:10 +0300

    wip: shares

--- contracts/BackroomShares.sol
+++ contracts/BackroomShares.sol
@@ -1,8 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-
-// TODO: Events, final pricing model,
 
 contract BackroomsShares is Ownable {
     address public protocolFeeDestination;

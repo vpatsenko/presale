fd074b9f4daeb478bffa5ca261d4e92275116836
vpatsenko <v@patsenko.xyz>
2025-09-13 16:02:55 +0300

    indexing stakers

--- scripts/getStakers.ts
+++ scripts/getStakers.ts
@@ -10,61 +10,89 @@ const STAKING_CONTRACT_ADDRESS = "0x36F6158ec445475B50387ECa09de977a8d194358";
 const STAKING_CONTRACT_ABI = [
 	"event Withdraw(address indexed user, uint pid, uint amount)",
 	"event Stake(address indexed user, uint id, uint amount, uint8 numWeeks)",
+	"event AutoRenew(address indexed user, uint256 id, bool autoRenew)",
 ]
 
 // const CREATION_BLOCK = 34745248;
 const CREATION_BLOCK = 31733060;
 const BLOCK_SIZE = 10000;
 
-const stakers = new Map<string, bigint>();
+
+interface Stake{
+	amount: bigint;
+	autoRenew: boolean;
+}
+
+const stakers = new Map<string, Map<bigint, Stake>>();
+const stakersCleared = new Map<string, bigint>();
 
 async function getStakers() {
 	const stakingContract = new ethers.Contract(STAKING_CONTRACT_ADDRESS, STAKING_CONTRACT_ABI, provider);
 
-	const withdrawFilter = stakingContract.filters.Withdraw();
 	const stakeFilter = stakingContract.filters.Stake();
+	const autoRenewFilter = stakingContract.filters.AutoRenew();
 
-
-
-
-
-	const latestBlock = await provider.getBlockNumber();
+	let latestBlock = await provider.getBlockNumber();
 	const blocksToProcess = latestBlock - CREATION_BLOCK;
 	const totalChunks = Math.ceil(blocksToProcess / BLOCK_SIZE);
 	let currentChunk = 0;
 
-	console.log(`Processing ${blocksToProcess} blocks in ${totalChunks} chunks`);
+
+	// latestBlock = CREATION_BLOCK +1_000;
+
 
 	for (let i = CREATION_BLOCK; i < latestBlock; i += BLOCK_SIZE) {
 		const endBlock = Math.min(i + BLOCK_SIZE, latestBlock);
 
-		let withdrawLogs = await stakingContract.queryFilter(withdrawFilter, i, endBlock);
 		let stakeLogs = await stakingContract.queryFilter(stakeFilter, i, endBlock);
+		let autoRenewLogs = await stakingContract.queryFilter(autoRenewFilter, i, endBlock);
 
 		stakeLogs.forEach((log: any) => {
 			const user = log.args.user;
+			const id = BigInt(log.args.id);
 			const amount = BigInt(log.args.amount);
 
-			stakers.set(user, (stakers.get(user) || BigInt(0)) + amount);
+			const stake: Stake = {
+				amount,
+				autoRenew: true,
+			};
+
+			stakers.set(user, (stakers.get(user) || new Map<bigint, Stake>()).set(id, stake));
 		});
 
-		withdrawLogs.forEach((log: any) => {
+		autoRenewLogs.forEach((log: any) => {
 			const user = log.args.user;
-			const amount = BigInt(log.args.amount);
+			const id = BigInt(log.args.id);
+			const autoRenew = log.args.autoRenew;
 
-			stakers.set(user, (stakers.get(user) || BigInt(0)) - amount);
+			console.log(`autoRenew ${user} ${id} ${autoRenew}`);
+
+			const stake = stakers.get(user)?.get(id);
+			if (stake) {
+				stake.autoRenew = autoRenew;
+			}
 		});
 
 		currentChunk++;
 		console.log(`Processed chunk ${currentChunk} of ${totalChunks}`);
 	}
+
+
+	stakers.forEach((stakes, address) => {
+		stakes.forEach((stake, id) => {
+			if (stake.autoRenew) {
+				stakersCleared.set(address, stakersCleared.get(address) || BigInt(0) + stake.amount);
+			}
+		});
+
+	});
 }
 
 async function saveStakers() {
 	let csvContent = "address,staked_amount\n";
 
-	Array.from(stakers.entries()).forEach(([address, amount]) => {
-		csvContent += `${address},${ethers.formatUnits(amount, 18)}\n`;
+	Array.from(stakersCleared.entries()).forEach(([address, amount]) => {
+		csvContent += `${address},${ethers.formatUnits(amount, 18)} \n`;
 	});
 
 	fs.writeFileSync("stakers.csv", csvContent);
--- scripts/withdrawFunds.ts
+++ scripts/withdrawFunds.ts
@@ -13,7 +13,7 @@ async function main(): Promise<void> {
 
 	console.log("- Owner:", owner.address);
 
-	const BackroomPresaleFactory = await ethers.getContractFactory("BackroomPresale");
+	const BackroomPresaleFactory = await ethers.getContractFactory("Presale");
 	const presale = await BackroomPresaleFactory.attach(BACKROOM_PRESALE_ADDRESS) as BackroomPresale;
 
 	console.log("\n1. Checking contract state:");
